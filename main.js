15dda976d6499c76e579935be60d52114be50ff9
Lewin Kelly <heli@odex.cf>
2023-06-16 01:31:26 +0100

    Use buffer module to decode base64 strings for better reliability

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-for (link of document.querySelectorAll("a"))
-	if (link.href.startsWith("https://www.bing.com/ck")) {
+import { Buffer } from "Buffer"
+
+for (const link of document.querySelectorAll("a"))
+	if (link.href.startsWith("https://www.bing.com/ck"))
 		try {
-			link.href = atob(
-				// Tracker parameters are base64 encoded at the end of the URL
-				link.href.split("&u=a1")[1].split("&ntb=1")[0]
-			)
+			// Tracker parameters are base64 encoded at the end of the URL
+			const trackerParams = link.href.split("&u=a1")[1].split("&ntb=1")[0]
+
+			link.href = Buffer.from(trackerParams, "base64").toString("utf-8")
 		} catch (e) {
 			link.style.background = "red"
+			console.error(e.message)
 		}
-	}
